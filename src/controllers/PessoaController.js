54776b9913a5dc041cbf55634dc22c5589a3802b
Raí Joia <megac154@gmail.com>
2024-01-29 15:56:30 -0300

    feat: getAll in controller pessoa

--- src/controllers/PessoaController.js
+++ src/controllers/PessoaController.js
@@ -1,9 +1,12 @@
+const database = require('../models');
+
 class PessoaController {
   static async getAll(req, res) {
     try {
-      // 
+      const listaDePessoa = await database.Pessoa.findAll();
+      return res.status(200).json(listaDePessoa); 
     } catch (error) {
-      // 
+      return res.status(500).json(error.message);
     }
   }
 }
